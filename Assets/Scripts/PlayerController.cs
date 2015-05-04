82a7acd29af121d3fd2b67a156f747048888857b
elzzup <guild0105@gmail.com>
2015-05-04 11:00:56 +0900

    initialize commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		speed = 10.0f;
+	}
+	
+	void FixedUpdate () {
+		float moveHorizonal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizonal, 0.0f, moveVertical);
+
+		rb.AddForce(movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "item") {
+			other.gameObject.SetActive(false);
+		}
+	}
+
+}
---
+++ Assets/Scripts/Rotater.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotater : MonoBehaviour {
+
+	int spinSpeed = 0;
+	void Update () {
+		spinSpeed += Random.Range(-10, 10);
+		transform.Rotate (new Vector3(15, spinSpeed, 30) * Time.deltaTime);
+	}
+}
